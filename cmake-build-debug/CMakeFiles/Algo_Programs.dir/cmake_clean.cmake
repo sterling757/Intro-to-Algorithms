695ed4533e6c0a4711ecb2a72bf03164d6ea92e9
Sterling Conner <saconner@smu.edu>
2018-08-24 19:29:28 -0500

    First Push

---
+++ cmake-build-debug/CMakeFiles/Algo_Programs.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Algo_Programs.dir/main.cpp.obj"
+  "Algo_Programs.pdb"
+  "Algo_Programs.exe"
+  "Algo_Programs.exe.manifest"
+  "libAlgo_Programs.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Algo_Programs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
