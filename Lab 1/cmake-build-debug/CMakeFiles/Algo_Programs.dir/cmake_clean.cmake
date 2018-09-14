fb8e0f3336ac5825d9ebfeafc48729670053bbc1
Sterling Conner <saconner@smu.edu>
2018-09-14 13:31:34 -0500

    reformatting

---
+++ Lab 1/cmake-build-debug/CMakeFiles/Algo_Programs.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Algo_Programs.dir/main.cpp.obj"
+  "CMakeFiles/Algo_Programs.dir/Algorithm.cpp.obj"
+  "CMakeFiles/Algo_Programs.dir/SortingAlgorithm.cpp.obj"
+  "CMakeFiles/Algo_Programs.dir/Sort.cpp.obj"
+  "Algo_Programs.pdb"
+  "Algo_Programs.exe"
+  "Algo_Programs.exe.manifest"
+  "libAlgo_Programs.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Algo_Programs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
